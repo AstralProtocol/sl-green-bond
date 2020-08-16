915b8e1302ce2726ebe4f2e59a4e06a8bec53191
Jared Childers <jared.childers97@gmail.com>
2020-08-16 11:31:07 -0400

    The Updated Store Data Javascript
    Will upload the CSV Files to the testnet or localnet;
    
    While the powergate localnet is running in Docker Container, run 'node storedata.js' to load the csv files. Save the Auth Token for later to make requests to retrieve the files from FFS.

--- backend/powergate/storedata.js
+++ backend/powergate/storedata.js
@@ -1,24 +1,23 @@
 const power = require("@textile/powergate-client");
 const fs = require("fs");
+const { create } = require("domain");
 const host = "http://localhost:6002"; // Powergate Instance PORT
-
-// Test File path 
-const filePath = `./csv-nox/london-mapnox2003.csv`;
-
-
-
 const pow = power.createPow({ host });
+    
+
 console.log(pow);
-generateAndSetAuthToken();
-checkNetworkHealthAndViewPeers();
+
+async function bindAuthToken() {
+    const { token } = await pow.ffs.create(); // save this token for later use!\
+    console.log(token);
+    // sets the authToken to the powergate instance
+    pow.setToken(token)
+}
 
 
-getPowerWalletAddressList()
-cacheData(filePath)
-storeDataInFFS(cid)
-watchJobStatus(jobId)
-sendFilTo(senderAddress, minerAddress, amountOfFil)
-
+//const authToken = '3a5b27c3-9c9d-4d6c-9f82-9c612803ad92';
+// sets the authToken to the instance
+//pow.setToken(authToken)
 
 // Generate and the Set the Auth Token 
 async function generateAndSetAuthToken() {
@@ -31,105 +30,82 @@ async function generateAndSetAuthToken() {
     }
 }
 
-/**
- *  @dev Checks the Network Health
- *  */ 
-async function checkNetworkHealthAndViewPeers () {
-    //const { status, messagesList } = await pow.health.check();
-    const { peersList } = await pow.net.peers()
-    return peersList;
-    
+// get general info about your ffs instance
+async function loader(){
+    var num = ["03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13"];
+    // Test File path 
+    for (var i = 0; i < num.length; i++){
+        const filePathBase = "./csv-nox/london-mapnox20" + num[i] + ".csv";
+        await loadTheDataToFFSMacroFunction(filePathBase);
+        await getInfoOnFFS();
+        console.log(filePathBase);
+    }
 }
 
 /**
- *  @dev Creates a new address associated with your FFS instance
- *  @returns {string} 
+ *  @dev get general info about your ffs instance
  * */ 
-async function createNewWallet(){
+async function getInfoOnFFS(){
     try{
-        const { addr } = await pow.ffs.newAddr("my new addr");
-    }
-    catch(error){
-        console.log(error);
-    }
-    return addr;
-}
-
-/**
- *  @dev Get wallet addresses associated with your FFS instance
- *  @returns {Array}
- * */
-async function getPowerWalletAddressList() { 
-    try{
-        const { addrsList } = await pow.ffs.addrs();
-    }
-    catch(error){
-        console.log(error);
-    }
-    return addrsList;
-}
-
-/** 
- *  @dev Cache data in IPFS in preparation to store it using FFS
- *  @param {string} filePath - The path of the file.
- *  @returns {string} 
- * */
-async function cacheData(filePath){
-    //const buffer = fs.readFileSync(`path/to/a/file`)
-    try{
-        const buffer = fs.readFileSync(filePath);
-        const { cid } = await pow.ffs.stage(buffer);
-    }
-    catch(error){
-        console.log(error);
-    }
-    return cid;
-}
-
-/** 
- *  @dev store the data in FFS using the default storage configuration
- *  @param {string} cid - The cid of the data
- * */
-async function storeDataInFFS(cid){
-    try{
-        const { jobId } = await pow.ffs.pushStorageConfig(cid);
+        const { info } = await pow.ffs.info();
+        console.log(info);
     }
     catch(error){
         console.log(error);
     } 
-    return jobId;
+    
 }
 
-/** 
- *  @dev send FIL from an address managed by your FFS instance to any other address
- *      [Note:] needs to be called before every request and after every retrieval of data from the miner
- *  @param {string} senderAddress - Address of the Sender
- *  @param {string} minderAddress - Address of the Miner 
- *  @param {string} amountOfFil - The Amount of FIL the Sender wants to send the Miner
- * */ 
-async function sendFilTo(senderAddress, minerAddress, amountOfFil) {
+async function loadTheDataToFFSMacroFunction(filePath){
+
     try{
-        await pow.ffs.sendFil(senderAddress, minerAddress, amountOfFil);
-    }catch(error){
+        // get wallet addresses associated with your FFS instance
+        const { addrsList } = await pow.ffs.addrs()
+
+        // create a new address associated with your ffs instance
+        //const { addr } = await pow.ffs.newAddr("minerAddress")
+            
+        // get general info about your ffs instance
+        //const { info } = await pow.ffs.info()
+        
+        // 3) Get use the file path to import the file and cache
+        const buffer = fs.readFileSync(filePath);
+        const { cid } = await pow.ffs.stage(buffer);
+        console.log(buffer);
+        console.log(cid);
+
+        // 4) store the Data in the FFS instance. We need to save the auth toke nto retrieve it again
+        const { jobId } = await pow.ffs.pushStorageConfig(cid);
+        
+        console.log(jobId);
+        const job = jobId;
+        
+        // 5) watch the Job status
+        /*
+        const jobsCancel = pow.ffs.watchJobs((job) => {
+            if (job.status === JobStatus.JOB_STATUS_CANCELED) {
+            console.log("job canceled")
+            } else if (job.status === JobStatus.JOB_STATUS_FAILED) {
+            console.log("job failed")
+            } else if (job.status === JobStatus.JOB_STATUS_SUCCESS) {
+            console.log("job success!")
+            }
+        }, jobId)
+
+        console.log(jobsCancel);*/
+        // 6) Send the Fil to the miner
+        //await pow.ffs.sendFil(addrsList[0].addr, addr, amountOfFil);
+    }
+    catch(error){
         console.log(error);
     }
+    
 }
 
-/** 
- *  @dev watch the FFS job status to see the storage process progressing
- *  @param {string} jobId - The job ID
- *  @returns {string}
- * */
-function watchJobStatus(jobId) {
-    const jobsCancel = pow.ffs.watchJobs((jobId) => {
-        if (job.status === JobStatus.JOB_STATUS_CANCELED) {
-          console.log("job canceled")
-        } else if (job.status === JobStatus.JOB_STATUS_FAILED) {
-          console.log("job failed")
-        } else if (job.status === JobStatus.JOB_STATUS_SUCCESS) {
-          console.log("job success!")
-        }
-      }, jobId)
-    
-    return jobsCancel;
-}
+
+// Runs the script 
+bindAuthToken();
+loader();
+
+
+
